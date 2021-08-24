31b5b3fc19bd2b80833e81c1909524ef7d1e3ae4
NaranjoDev <49539139+jnaranjoDev@users.noreply.github.com>
2021-08-24 14:50:12 +0200

    practica1

---
+++ src/main/java/com/mybank/domain/Account.java
@@ -0,0 +1,54 @@
+package com.mybank.domain;
+
+import java.io.Serializable;
+
+public class Account implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3717131186762238385L;
+	private double balance;
+
+	public Account(double initBalance) {
+		super();
+		this.balance = initBalance;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+	
+	public boolean deposit(double amt) {
+		if((this.balance + amt) < this.balance) {
+			System.out.println("No puedes depositar en negativo");
+			return false;
+		}else {
+			System.out.println("Dinero depositado");
+			System.out.println("El saldo actual es: "+this.balance);
+			this.balance += amt;
+			return true;
+		}
+		
+	}
+	
+	public boolean withdraw(double amt) {
+		if( (this.balance - amt) < 0) {
+			System.out.println("No puedes retirar tanto dinero");
+			System.out.println("El saldo actual es: "+this.balance);
+			return false;
+		}else {
+			this.balance -= amt;
+			System.out.println("Dinero retirado");
+			System.out.println("El saldo actual es: "+this.balance);
+			return true;
+		}
+		
+		
+		
+	}
+	
+	
+	
+	
+}
